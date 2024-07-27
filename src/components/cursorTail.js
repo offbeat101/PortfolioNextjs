a592886da982304d14452a7af489756b8640652f
Yash <122795856+offbeat101@users.noreply.github.com>
2024-07-27 16:22:45 +0530

    Update cursorTail.js

--- src/components/cursorTail.js
+++ src/components/cursorTail.js
@@ -1,17 +1,17 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useMemo } from 'react';
 import styles from './cursorTail.module.css'; // Adjust the import according to your setup
 
 const CursorTail = () => {
   const coords = useRef({ x: 0, y: 0 });
   const circlesRef = useRef([]);
 
-  const colors = [
+  const colors = useMemo(() => [
     "#ffb56b", "#fdaf69", "#f89d63", "#f59761", "#ef865e",
     "#ec805d", "#e36e5c", "#df685c", "#d5585c", "#d1525c",
     "#c5415d", "#c03b5d", "#b22c5e", "#ac265e", "#9c155f",
     "#950f5f", "#830060", "#7c0060", "#680060", "#60005f",
     "#48005f", "#3d005e"
-  ];
+  ], []);
 
   useEffect(() => {
     const circles = circlesRef.current;
